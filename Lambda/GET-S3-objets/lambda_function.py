d2c7efddaf05720e8899ea246a02561824ad5caa
kfgmartinez <129803275+kfgmartinez@users.noreply.github.com>
2023-08-28 23:34:21 -0600

    Update lambda_function.py

--- Lambda/GET-S3-objets/lambda_function.py
+++ Lambda/GET-S3-objets/lambda_function.py
@@ -1,21 +1,25 @@
 import base64
 import boto3
 
-
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    bucket_name = event ["pathParameters"]["bucket"]
-    file_name = event ["queryStringParameters"]["file"]
-    fileObj = s3.get_object(Bucket=bucket_name, Key=file_name) #boto3 module
-    file_content = fileObj["Body"].read()
-    # print
+    bucket_name = event["pathParameters"]["bucket"]
+    file_name = event["queryStringParameters"]["file"]
+
+    response = s3.get_object(Bucket=bucket_name, Key=file_name)
+    file_content = response["Body"].read()
+
+    base64_encoded_content = base64.b64encode(file_content).decode('utf-8')
+
+    headers = {
+        "Content-Type": "application/jpg",
+        "Content-Disposition": "attachment; filename={}".format(file_name)
+    }
+
     return {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/jpg",
-            "Content-Disposition": f"attachment; filename={file_name}"
-        },
-        "body": base64.b64encode(file_content), #encode the response with base64
+        "headers": headers,
+        "body": base64_encoded_content,
         "isBase64Encoded": True
     }
